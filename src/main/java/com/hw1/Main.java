b29202a080b9b7c656e888c640232905c8ded7c8
veronika.genkinova <veranika-2005@ukr.net>
2025-05-09 19:36:27 +0300

    m

--- src/main/java/com/hw1/Main.java
+++ src/main/java/com/hw1/Main.java
@@ -3,6 +3,14 @@ package main.java.com.hw1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello git!");
+        int side1 = 3;
+        int side2 = 5;
+        int side3 = 7;
+
+        int volume = side1 * side2 * side3;
+        int length = (side1 + side2 + side3) * 4;
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Довжина всіх сторін паралелепіпеда = " + length);
     }
 }
